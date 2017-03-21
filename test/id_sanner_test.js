1142b38120dedaefb2b6cfba0d6cc4a0fdb4b232
Ming Jin <skyairmj@gmail.com>
2017-03-21 12:06:13 +0800

    Ming Jin: completed id photo parsing case

---
+++ test/id_sanner_test.js
@@ -0,0 +1,53 @@
+var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+var https = require('https');
+
+describe('IDScanner', function() {
+  describe('#scan()', function() {
+    it('should parse personal information', function (done) {
+      var file_path = path.join(__dirname, '/fixtures/idcard_example.jpg');
+      fs.readFile(file_path, (err, data) => {
+        if (err) {
+          console.error(err);
+          done(err);
+        }
+        var image_data = new Buffer(data).toString('base64');
+
+        var content = "{\"inputs\":[{\"image\":{\"dataType\":50,\"dataValue\":\""+image_data+"\"},\"configure\":{\"dataType\":50,\"dataValue\":\"{\\\"side\\\":\\\"face\\\"}\"}}]}"
+
+        const CONFIG = require(path.join(__dirname, '/support/config.js')).config;
+        var appcode = CONFIG.aliyun.api.appcode;
+
+        var options = {
+          host: 'dm-51.data.aliyun.com',
+          path: '/rest/160601/ocr/ocr_idcard.json',
+          method: 'POST',
+          rejectUnauthorized: false,
+          headers: {
+            'Authorization': 'APPCODE ' + appcode,
+            'Content-Type': 'application/json; charset=UTF-8'
+          }
+        };
+
+        var req = https.request(options, (res) => {
+          res.on('data', (chunk) => {
+            var str = chunk.toString();
+            var result = JSON.parse(str);
+            console.log(result.outputs[0].outputValue.dataValue);
+          }).on('end', () => {
+            done();
+          });
+        });
+
+        req.on('error', (err) => {
+          console.error(err);
+          done(err);
+        });
+
+        req.write(content);
+        req.end();
+      });
+    });
+  });
+});
