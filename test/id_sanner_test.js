8f5c538f737914f2f6f651d15f3fcaa8ffa0780d
Ming Jin <skyairmj@gmail.com>
2017-03-21 13:22:21 +0800

    Ming Jin: completed org license parsing

--- test/id_sanner_test.js
+++ test/id_sanner_test.js
@@ -49,5 +49,50 @@ describe('IDScanner', function() {
         req.end();
       });
     });
+
+
+    it('should parse license information', function (done) {
+      var file_path = path.join(__dirname, '/fixtures/license_example.jpg');
+      fs.readFile(file_path, (err, data) => {
+        if (err) {
+          console.error(err);
+          done(err);
+        }
+        var image_data = new Buffer(data).toString('base64');
+
+        var content = "{\"inputs\":[{\"image\":{\"dataType\":50,\"dataValue\":\""+image_data+"\"}}]}"
+
+        var appcode = config.aliyun.api.appcode;
+
+        var options = {
+          host: 'dm-58.data.aliyun.com',
+          path: '/rest/160601/ocr/ocr_business_license.json',
+          method: 'POST',
+          rejectUnauthorized: false,
+          headers: {
+            'Authorization': 'APPCODE ' + appcode,
+            'Content-Type': 'application/json; charset=UTF-8'
+          }
+        };
+
+        var req = https.request(options, (res) => {
+          res.on('data', (chunk) => {
+            var str = chunk.toString();
+            var result = JSON.parse(str);
+            console.log(result.outputs[0].outputValue.dataValue);
+          }).on('end', () => {
+            done();
+          });
+        });
+
+        req.on('error', (err) => {
+          console.error(err);
+          done(err);
+        });
+
+        req.write(content);
+        req.end();
+      });
+    });
   });
 });
