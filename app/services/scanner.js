2c1fecc62355b8e5a02f496a3ac663a1aabd8b7c
Ming Jin <skyairmj@gmail.com>
2017-03-25 00:50:14 +0800

    Ming Jin: refactor Scanner class to add image file reading logic

--- app/services/scanner.js
+++ app/services/scanner.js
@@ -1,40 +1,43 @@
+var fs = require('fs');
 var config = require('config');
 var https = require('https');
 
 module.exports = function(){
 
-  var _scan = function () {
+  var _scan = function (imagePath, strategy, dataHandler, errHandler) {
+    fs.readFile(imagePath, (err, image) => {
+      if (err) {
+        console.error(err);
+        errHandler(err);
+      }
 
-  };
+      var image_data = new Buffer(image).toString('base64');
+      var content = strategy.getContent(image_data);
 
-  var _parse = function (image, strategy, dataHandler, errorHandler) {
-    var image_data = new Buffer(image).toString('base64');
-    var content = strategy.getContent(image_data);
+      var appcode = config.aliyun.api.appcode;
+      var options = strategy.getOptions(appcode);
 
-    var appcode = config.aliyun.api.appcode;
-    var options = strategy.getOptions(appcode);
-
-    var req = https.request(options, res => {
-      res.on('data', chunk => {
-        var str = chunk.toString();
-        console.log(str);
-        var result = strategy.getResult(str);
-        dataHandler(result);
-      }).on('end', () => {
+      var req = https.request(options, res => {
+        res.on('data', chunk => {
+          var str = chunk.toString();
+          console.log(str);
+          var result = strategy.getResult(str);
+          dataHandler(result);
+        }).on('end', () => {
+        });
       });
-    });
 
-    req.on('error', err => {
-      console.error(err);
-      errorHandler(err);
-    });
+      req.on('error', err => {
+        console.error(err);
+        errHandler(err);
+      });
 
-    req.write(content);
-    req.end();
+      req.write(content);
+      req.end();
+    });
   };
 
   return {
-    scan: _scan,
-    parse: _parse
+    scan: _scan
   };
 }();
