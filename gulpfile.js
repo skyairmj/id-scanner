9debe32e4284a13e9d9fd242790976caef65a71b
Ming Jin <skyairmj@gmail.com>
2017-03-26 16:32:08 +0800

    Ming Jin: initiate build automation

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+
+gulp.task('html', function(){
+});
+
+gulp.task('js', function(){
+});
+
+gulp.task('css', function(){
+});
+
+gulp.task('bower', function(){
+});
+
+gulp.task('default', [ 'html', 'js', 'css', 'bower' ]);
